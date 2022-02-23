0a7c0bb732b90c229b2144c92dcbf93726f72d68
suvanlly <swl381029812@gmail.com>
2022-02-24 10:06:05 +1100

    edit comments

--- src/components/Subtitle/Subtitle.jsx
+++ src/components/Subtitle/Subtitle.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { SubtitleWrapper } from "./styles/subtitle";
 
 const Subtitle = ({ title, id }) => (
-  // received title and datatestid from parent component (Greyhound, Harness, Thoroughbred)
+  // received title and id from parent component (Greyhound, Harness, Thoroughbred)
   <SubtitleWrapper id={id}>
     {title}
   </SubtitleWrapper>
